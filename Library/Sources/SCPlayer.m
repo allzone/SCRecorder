b19c0af9f6297534da7a2e6e38500b363b79eeec
Ion Brumaru <>
2020-07-06 19:27:47 +0300

    Set default frames per second

--- Library/Sources/SCPlayer.m
+++ Library/Sources/SCPlayer.m
@@ -228,7 +228,6 @@ static char* LoadedTimeRanges = "LoadedTimeRanges";
         SCWeakSelectorTarget *target = [[SCWeakSelectorTarget alloc] initWithTarget:self targetSelector:@selector(willRenderFrame:)];
 
         _displayLink = [CADisplayLink displayLinkWithTarget:target selector:target.handleSelector];
-        _displayLink.preferredFramesPerSecond = 1;
 
         [self setupVideoOutputToItem:self.currentItem];
 
